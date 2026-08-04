c5f927d7ad2070c998d6b49b8ce7c6051f5c735b
Freya <3339085482@qq.com>
2026-08-04 13:05:52 +0800

    feat: add two friend links

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -45,6 +45,22 @@ export const friendsData: FriendItem[] = [
 		siteurl: "https://ka1saaa.github.io/",
 		tags: ["Misc", "随笔"],
 	},
+	{
+		id: 5,
+		title: "实验室网安组佬",
+		imgurl: "https://github.com/dev-usernamehao.png?size=200",
+		desc: "网络安全实验室站点",
+		siteurl: "https://dev-usernamehao.github.io/",
+		tags: ["网安", "实验室"],
+	},
+	{
+		id: 6,
+		title: "开发佬",
+		imgurl: "https://ui-avatars.com/api/?name=开发佬&background=4f46e5&color=fff&size=200",
+		desc: "开发相关站点",
+		siteurl: "http://47.105.98.148:4321/",
+		tags: ["开发"],
+	},
 ];
 
 // 获取所有友情链接数据
